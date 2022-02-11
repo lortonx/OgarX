9262338a39e819111a9ba4cae63aa1203eb680df
neo <lortonx@gmail.com>
2022-02-11 10:23:46 +0200

    upd

--- src/modes/custom/selfeed.js
+++ src/modes/custom/selfeed.js
@@ -1,39 +1,39 @@
 /** @type {typeof import("../../physics/engine").DefaultSettings} */
 module.exports = {
-    TIME_SCALE: 1.2, // magic that make everything work like a certain ball game
+    TIME_SCALE: 1,//1.2, // magic that make everything work like a certain ball game
 
     PLAYER_MAX_CELLS: 32,
-    PLAYER_MERGE_NEW_VER: false,
+    PLAYER_MERGE_NEW_VER: true,
     PLAYER_AUTOSPLIT_SIZE: 2000,
     PLAYER_MERGE_TIME: 0,
-    PLAYER_NO_MERGE_DELAY: 800,//900
-    PLAYER_SPEED: 1.2,
+    PLAYER_NO_MERGE_DELAY: 900,//900
+    PLAYER_SPEED: 0.8,
 
     VIRUS_COUNT: 40,
     VIRUS_SIZE: 125,
     VIRUS_PUSH: true,
     VIRUS_MONOTONE_POP: false,
 
-    EJECT_SIZE: 48,//85,
-    EJECT_LOSS: 43,//81
+    EJECT_SIZE: 38,//85,
+    EJECT_LOSS: 33,//81
     EJECT_DELAY: 40,
 
     BOTS: 10, 
-    BOT_SPAWN_SIZE: 400,
-    PELLET_COUNT: 3000,
+    BOT_SPAWN_SIZE: 600,
+    PELLET_COUNT: 2000,
     PLAYER_VIEW_SCALE: 1.2,
     PLAYER_SPAWN_SIZE: 500,
     PLAYER_SPAWN_DELAY: 10,
     PLAYER_SPLIT_DIST: 80,
-    PLAYER_MIN_SPLIT_SIZE: 150,
-    PLAYER_MIN_EJECT_SIZE: 90,
+    PLAYER_MIN_SPLIT_SIZE: 100,//150,
+    PLAYER_MIN_EJECT_SIZE: 50,//90,
 
     DECAY_MIN: 200,
     STATIC_DECAY: 15,
     DYNAMIC_DECAY: 0.3,
     DUAL_ENABLED: false,
     PLAYER_AUTOSPLIT_DELAY: 5,
-    PLAYER_VIEW_SCALE: 1,
+    PLAYER_VIEW_SCALE: 0.8,
     MAP_HW: 10071,
     MAP_HH: 10071,
     NORMALIZE_THRESH_MASS: 100000000,//100000
