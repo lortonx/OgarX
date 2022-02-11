79814ae560333fc0b9b124b81b2f972f2a5df05f
neo <lortonx@gmail.com>
2022-02-12 01:59:10 +0200

    fx

---
+++ src/modes/custom/selfeed2.js
@@ -0,0 +1,30 @@
+/** @type {typeof import("../../physics/engine").DefaultSettings} */
+module.exports = {
+    TIME_SCALE: 1.2, // magic that make everything work like a certain ball game
+    PLAYER_MAX_CELLS: 128,
+    PLAYER_MERGE_NEW_VER: true,
+    PLAYER_AUTOSPLIT_SIZE: 0,
+    PLAYER_MERGE_TIME: 0,
+    PLAYER_NO_MERGE_DELAY: 900,
+    PLAYER_SPEED: 2,
+    VIRUS_COUNT: 0,
+    VIRUS_SIZE: 200,
+    VIRUS_PUSH: true,
+    VIRUS_MONOTONE_POP: true,
+    EJECT_SIZE: 85,
+    EJECT_LOSS: 80,
+    EJECT_DELAY: 100,
+    BOTS: 40,
+    BOT_SPAWN_SIZE: 1000,
+    PELLET_COUNT: 2000,
+    PLAYER_VIEW_SCALE: 1.2,
+    PLAYER_SPAWN_SIZE: 1500,
+    PLAYER_SPAWN_DELAY: 1500,
+    PLAYER_SPLIT_DIST: 80,
+    PLAYER_MIN_SPLIT_SIZE: 150,
+    PLAYER_MIN_EJECT_SIZE: 100,
+    MAP_HW: 26000,
+    MAP_HH: 26000,
+    NORMALIZE_THRESH_MASS: 100000,
+    PLAYER_SAFE_SPAWN_RADIUS: 1.2
+};
