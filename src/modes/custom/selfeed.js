3597c743d9e6eaf46a10894265ba790d8a40a88b
neo <lortonx@gmail.com>
2022-02-13 23:59:34 +0200

    fix

--- src/modes/custom/selfeed.js
+++ src/modes/custom/selfeed.js
@@ -3,11 +3,13 @@ module.exports = {
     // Like Arctida
     TIME_SCALE: 1,//1.2, // magic that make everything work like a certain ball game
     PLAYER_MAX_CELLS: 64,
-    PLAYER_MERGE_NEW_VER: false,
+    PLAYER_MERGE_NEW_VER: true,
     PLAYER_AUTOSPLIT_SIZE: 2000,
     PLAYER_MERGE_TIME: 0,
     PLAYER_NO_MERGE_DELAY: 900,//900
-    PLAYER_SPEED: 0.7,
+    PLAYER_NO_EJECT_DELAY:0,
+
+    PLAYER_SPEED: 0.8,
 
     PHYSICS_TPS: 20,
     MAX_CELL_PER_TICK: 50,
@@ -15,14 +17,15 @@ module.exports = {
     VIRUS_COUNT: 40,
     VIRUS_SIZE: 100,
     VIRUS_PUSH: false,
-    VIRUS_SPLIT_BOOST: 1020,
+    VIRUS_SPLIT_BOOST: 1220,
     VIRUS_MONOTONE_POP: false,
-    VIRUS_FEED_TIMES: 7,
+    VIRUS_FEED_TIMES: 6,
     VIRUS_MAX_BOOST: 5000, // разлетание частиц при попе
 
-    EJECT_SIZE: 35,//85,
+    EJECT_SIZE: 38,//85,
     EJECT_LOSS: 33,//81
-    EJECT_DELAY: 40,
+    EJECT_DELAY: 135,
+    EJECT_DISPERSION: 0.25,
 
     BOTS: 15, 
     BOT_SPAWN_SIZE: 600,
@@ -31,13 +34,14 @@ module.exports = {
     PLAYER_SPAWN_SIZE: 500,
     PLAYER_SPAWN_DELAY: 10,
     PLAYER_SPLIT_DIST: 80,
-    PLAYER_NO_COLLI_DELAY: 750,// пружинит при сплите
+    PLAYER_NO_COLLI_DELAY: 650,// пружинит при сплите
     PLAYER_MIN_SPLIT_SIZE: 120,//150,
     PLAYER_MIN_EJECT_SIZE: 64,//80,//90,
-    PLAYER_NO_EJECT_POP_DEALY: 500,
+    PLAYER_NO_EJECT_POP_DEALY: 100,
     // PLAYER_MERGE_INCREASE: 100,
     PLAYER_VIEW_SCALE: 0.49,
     PLAYER_VIEW_MIN: 2000,
+    PLAYER_SPLIT_BOOST:800,
 
     DECAY_MIN: 200,
     STATIC_DECAY: 15,
